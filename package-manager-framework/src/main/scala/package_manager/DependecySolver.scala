3b033c9aa209cadcf6fd90c3ec46680a0da1bfa2
sh4869 <nobuk4869@gmail.com>
2021-06-13 14:36:46 +0900

    コンパイルが通るように修正

--- package-manager-framework/src/main/scala/package_manager/DependecySolver.scala
+++ package-manager-framework/src/main/scala/package_manager/DependecySolver.scala
@@ -1,5 +1,7 @@
 package package_manager
 
+import scala.collection.mutable.ListBuffer
+
 trait DependencySolver[DependencyType <: Dependency] {
   protected val packageManagerOption: PackageManagerOption
   protected val packageRegistry: PackageRegistry[DependencyType]
@@ -14,18 +16,22 @@ trait DependencySolver[DependencyType <: Dependency] {
       installed: Seq[PackageInfo[DependencyType]]
   ): Set[InstallPackage] = {
     packageRegistry.update
-    var newPackages = scala.collection.mutable.ListBuffer.empty[PackageInstallRequest]
-    var remove = scala.collection.mutable.ListBuffer.empty[PackageInfo[DependencyType]]
+    var newPackages = ListBuffer.empty[PackageInstallRequest]
+    var remove = ListBuffer.empty[PackageInfo[DependencyType]]
     installed.foreach(pack => {
       val packages = packageRegistry.getPackages(pack.name)
-      val newPackage = packages
+      packages
         .filter(p => p.version.isNewerThan(pack.version))
         .sortWith((a, b) => a.version.isNewerThan(b.version))
-        .headOption
-      if (newPackage.nonEmpty) {
-        // newPackages.addOne(PackageInstallRequest(newPackage.get.baseInfo.name, newPackage.get.baseInfo.version))
-        remove += pack
-      }
+        .collect {
+          case newPackage => {
+            newPackages += PackageInstallRequest(
+              newPackage.baseInfo.name,
+              newPackage.baseInfo.version
+            )
+            remove += pack
+          }
+        }
     })
     solve(newPackages.toSeq, remove.toSeq, installed)
   }
--- package-manager-framework/src/main/scala/package_manager/VersionRange.scala
+++ package-manager-framework/src/main/scala/package_manager/VersionRange.scala
@@ -1,5 +1,13 @@
 package package_manager
 
 trait VersionRange {
-    def acceptVersions(list: Seq[Version]): Seq[Version]
+  def acceptVersions(list: Seq[Version]): Seq[Version]
+}
+
+object VersionRange {
+  // version -> VersionRangeへのimplicit
+  implicit class OneVersionRange(val version: Version) extends VersionRange {
+    def acceptVersions(list: Seq[Version]): Seq[Version] =
+      list.filter(_ == version)
+  }
 }
